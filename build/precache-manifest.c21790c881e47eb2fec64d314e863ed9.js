b2d6a26f303c083a7c5836b68a4e8c4e9f1aab35
Joseph Carey <josephcareycomposer@gmail.com>
2018-11-28 15:36:46 -0600

    build

---
+++ build/precache-manifest.c21790c881e47eb2fec64d314e863ed9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84d483d76d155c7a8742",
+    "url": "/static/js/main.84d483d7.chunk.js"
+  },
+  {
+    "revision": "070f78412adcd601aa03",
+    "url": "/static/css/1.27cd06d0.chunk.css"
+  },
+  {
+    "revision": "070f78412adcd601aa03",
+    "url": "/static/js/1.070f7841.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "567372794d5380f84da19778f0181366",
+    "url": "/index.html"
+  }
+];
